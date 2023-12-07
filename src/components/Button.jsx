a914e4a2c6baf986a0c97c7c04abc6c0052c7ae7
Anton949432 <antonbratcikov@gmail.com>
2023-12-07 19:42:26 +0200

    ff

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Button = ({ onClick }) => {
+    return (
+        <button type="button" className="button" onClick={onClick}>
+            Load more
+        </button>
+    );
+};
+
+export default Button;
+
